bfaa41c5c16d7bbfca61c0673416d4dddaa329af
Sineth Wickramaratna <sinethwickramaratna@gmail.com>
2026-02-27 19:47:49 +0530

    feat: Update index.html title and add CORS middleware in main.py; create vercel.json for API rewrites

--- Server/app/main.py
+++ Server/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import movies, categories, languages
 
 app = FastAPI(title="Movie Recommender API", description="An API for recommending movies based on user preferences.", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(movies.router, prefix="/api/movies", tags=["Movies"])
 app.include_router(categories.router, prefix="/api/categories", tags=["Categories"])
